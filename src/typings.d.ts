3a7dcc61264f0306787c4753dc0363e87d0e93be
Kamil Kisiela <kamilkisiela@users.noreply.github.com>
2016-09-16 13:35:44 +0200

    Use typed-graphql for declarations (#51)

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -2,7 +2,7 @@
 // https://github.com/typings/typings
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
 
-/// <reference types="graphql-typings" />
+/// <reference types="typed-graphql" />
 
 declare module 'node-emoji' {
   export function emojify(input: string): string;
