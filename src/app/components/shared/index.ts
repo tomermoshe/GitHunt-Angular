059b9667d6704c38f63ae16dad7d594602c3c91f
Boguslaw Skrzypkowiak <skrzypkowiak@gmail.com>
2016-10-23 09:46:41 +0400

    type added, angular downgraded to 2.1.0 because of this issue: https://github.com/angular/angular-cli/issues/2812

--- src/app/components/shared/index.ts
+++ src/app/components/shared/index.ts
@@ -1,8 +1,9 @@
 import { InfoLabelComponent } from './info-label.component';
 import { LoadingComponent } from './loading.component';
 import { RepoInfoComponent } from './repo-info.component';
+import { Component } from '@angular/core';
 
-export const SHARED_DECLARATIONS = [
+export const SHARED_DECLARATIONS: Component[] = [
   InfoLabelComponent,
   LoadingComponent,
   RepoInfoComponent
